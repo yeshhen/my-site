e65799a16597b7be89379683bb3ed26d59d038b1
yeshhen <ypate008@gmail.com>
2026-05-01 23:45:21 -0700

    removals

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('open-modal').addEventListener('click', function(e) {
-  e.preventDefault();
-  document.getElementById('my-modal').classList.add('active');
-});
-
-document.getElementById('close-modal').addEventListener('click', function() {
-  document.getElementById('my-modal').classList.remove('active');
-});
